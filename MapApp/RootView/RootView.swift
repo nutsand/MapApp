856bfbb43039657b2349e0c441374f73e33d3e37
nutsand <nmimosan@gmail.com>
2021-08-06 20:10:41 +0900

    RootViewのViewModelを別ファイルへと分離

--- MapApp/RootView/RootView.swift
+++ MapApp/RootView/RootView.swift
@@ -40,26 +40,3 @@ struct RootView_Previews: PreviewProvider {
         RootView(vm: RootViewModel(isPreview: true))
     }
 }
-
-class RootViewModel: ObservableObject {
-    let cdmanager: CoreDataManager
-    @Published var roots: [Root] = []
-    
-    init(isPreview: Bool) {
-        if isPreview {
-            cdmanager = CoreDataManager.preview
-        } else {
-            cdmanager = CoreDataManager.shared
-        }
-        getRoot()
-    }
-    
-    func getRoot() {
-        let request = NSFetchRequest<Root>(entityName: "Root")
-        do {
-           roots = try cdmanager.context.fetch(request)
-        } catch let error {
-            print("fetch error...\(error.localizedDescription)")
-        }
-    }
-}
---
+++ MapApp/RootView/RootViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  RootViewModel.swift
+//  MapApp
+//
+//  Created by 内藤光博 on 2021/08/06.
+//
+
+import Foundation
+import CoreData
+
+class RootViewModel: ObservableObject {
+    let cdmanager: CoreDataManager
+    @Published var roots: [Root] = []
+    
+    init(isPreview: Bool) {
+        if isPreview {
+            cdmanager = CoreDataManager.preview
+        } else {
+            cdmanager = CoreDataManager.shared
+        }
+        getRoot()
+    }
+    
+    func getRoot() {
+        let request = NSFetchRequest<Root>(entityName: "Root")
+        do {
+           roots = try cdmanager.context.fetch(request)
+        } catch let error {
+            print("fetch error...\(error.localizedDescription)")
+        }
+    }
+}
